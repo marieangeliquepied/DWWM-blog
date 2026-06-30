18f8be45d9023cb73e6bf911dbf14f26a6381e8d
Marie-Angelique <m.angelique.pied@outlook.fr>
2026-06-30 10:00:45 -0400

    fix: implémentation de la méthode index dans ArticleController pour afficher la liste des articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
-class ArticleController extends Controller
-{
-    //
+class ArticleController extends Controller{
+
+    public function index(): View {
+        $articles = Article::all();
+
+        return view('articles-list', ['articles' => $articles]);
+    }
 }
