a8b5a0290947ac9688af298164faa7c412a61cd1
Marie-Angelique <m.angelique.pied@outlook.fr>
2026-06-30 10:36:24 -0400

    Add : création de la vue pour afficher la liste des articles

---
+++ resources/views/articles-list.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Article list</title>
+</head>
+<body>
+    <h1>Articles list</h1>
+
+    @foreach ($articles as $article)
+        <div>
+            <h2>{{ $article->title }}</h2>
+            <p>{{ $article->content }}</p>
+        </div>
+    @endforeach
+</body>
+</html>
