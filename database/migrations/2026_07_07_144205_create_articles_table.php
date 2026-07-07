c19c6eb7781a2b8903242f7b3a34150228377018
Marie-Angelique <m.angelique.pied@outlook.fr>
2026-07-07 16:06:37 -0400

    feat: maj de la migration pour la table articles avec les champs nécessaires

--- database/migrations/2026_07_07_144205_create_articles_table.php
+++ database/migrations/2026_07_07_144205_create_articles_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->enum('status',['DRAFT','PUBLISHED'])->default('DRAFT');
             $table->timestamp('published_at')->nullable();
             $table->timestamps();
+            $table->foreignId('category_id')->constrained()->restrictOnDelete();
+            $table->foreignId('user_id')->constrained()->restrictOnDelete();
         });
     }
 
