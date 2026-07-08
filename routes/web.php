8ca06bf6dc90c556d95dcfd0677bc35a05566835
Marie-Angelique <m.angelique.pied@outlook.fr>
2026-07-08 10:03:05 -0400

    feat: ajout des routes pour le contrôleur AdminController et mise à jour des imports

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
+use App\Models\Category;
 
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/admin/articles', [AdminController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index']);
