99819813375a5992586b2ff085f2838c97f84303
Marie-Angelique <m.angelique.pied@outlook.fr>
2026-07-21 08:41:53 -0400

    feat: model category complete with fillable and relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Article;
 
-class Category extends Model
-{
-    //
+class Category extends Model{
+    // Liaison avec la base de données
+    protected $table = 'categories';
+
+    // Liste des colonnes autorisées à être remplies automatiquement
+    // lors de l'enregistrement du formulaire
+    protected $fillable = [
+        'name', 'slug'
+    ];
+
+    // Relation avec la table article
+    public function articles() : HasMany {
+        return $this->hasMany(Article::class);
+    }
 }
