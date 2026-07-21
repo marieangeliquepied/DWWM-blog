82e20d6a163a25fc84c5e2962e98474912dff610
Marie-Angelique <m.angelique.pied@outlook.fr>
2026-07-21 11:11:51 -0400

    feat: mise à jour de la vue des catégories pour l'admin

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,12 +11,19 @@ class CategoryController extends Controller
     public function index() : View {
         $categories = Category::all();
 
-        return view('categories-list', ['categories' => $categories]);
+        return view('admin-categories-list', ['categories' => $categories]);
     }
 
     public function getCategoryById(int $id) {
         $category = Category::findOrFail($id);
 
         return response()->json($category);
+    }
+
+    public function adminIndex(): View {
+    // On récupère les catégories avec le nombre d'articles associés
+    $categories = Category::withCount('articles')->get();
+
+    return view('admin-categories-list', ['categories' => $categories]);
     }
 }
